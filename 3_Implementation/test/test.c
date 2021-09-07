10691c657babf79a18dde6a54ea47eeb28ec34b9
klsravani123 <89509994+klsravani123@users.noreply.github.com>
2021-09-07 13:10:50 +0530

    Create test.c

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,28 @@
+#include <foo.h>
+#include "unity.h"
+
+
+/* Modify these two lines according to the project */
+#define PROJECT_NAME    "CashFlow "
+
+/* Prototypes for all the test functions */
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+
+int main(void)
+{
+    /* Initiate the Unity Test Framework */
+    UNITY_BEGIN();
+
+    /* Run Test functions */
+    
+
+
+    /* Close the Unity Test Framework */
+    return UNITY_END();
+}
